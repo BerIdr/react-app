95481f0ed078f4a4c2480c256cf144e6c64ed768
Your Name <you@example.com>
2025-03-18 11:15:30 +0200

    updated forms

---
+++ src/views/Layout.tsx
@@ -0,0 +1,41 @@
+import {Link, Outlet} from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <>
+      <h1>My App</h1>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/Profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/Upload">Upload</Link>
+            </li>
+            <li>
+              <Link to="/Login">Login</Link>
+            </li>
+            <li>
+              <Link to="/Register">Register</Link>
+            </li>
+            <li>
+              <Link to="/Logout">Logout</Link>
+            </li>
+          </ul>
+        </nav>
+        <main>
+          <Outlet />
+        </main>
+        <footer>
+          <p>&copy; 2025</p>
+        </footer>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
